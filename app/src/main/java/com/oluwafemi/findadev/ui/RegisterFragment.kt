25ac70d7035422def2601e70c3ddba6524f19375
dev-juri <ojurifemi132@gmail.com>
2020-10-09 22:52:50 +0100

    feat: completed UI for registration

--- app/src/main/java/com/oluwafemi/findadev/ui/RegisterFragment.kt
+++ app/src/main/java/com/oluwafemi/findadev/ui/RegisterFragment.kt
@@ -1,14 +1,23 @@
 package com.oluwafemi.findadev.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.oluwafemi.findadev.R
+import com.oluwafemi.findadev.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
 
+    private lateinit var binding: FragmentRegisterBinding
+    private val techStacks = arrayListOf(
+        "Front-end", "Back-end", "DevOps", "Mobile(Native)",
+        "Mobile(Cross platform)", "UI/UX", "Data Science", "ML/DL"
+    )
+    private val jobType = arrayListOf("Full-Time", "Remote", "Contract")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +27,17 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_register, container, false)
+
+        val stackAdapter = ArrayAdapter(this.requireContext(), R.layout.spinner_list, techStacks)
+        stackAdapter.setDropDownViewResource(R.layout.spinner_list)
+        binding.selectStack.adapter = stackAdapter
+
+        val jobAdapter = ArrayAdapter(this.requireContext(), R.layout.spinner_list, jobType)
+        jobAdapter.setDropDownViewResource(R.layout.spinner_list)
+        binding.jobType.adapter = jobAdapter
+
+        return binding.root
     }
 
 }
