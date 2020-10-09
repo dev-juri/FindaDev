c0718662c2078e6542a513fb1de67672a2ace5ba
dev-juri <ojurifemi132@gmail.com>
2020-10-09 17:28:22 +0100

    Setup bottom navigation

--- app/src/main/java/com/oluwafemi/findadev/MainActivity.kt
+++ app/src/main/java/com/oluwafemi/findadev/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.oluwafemi.findadev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.oluwafemi.findadev.databinding.ActivityMainBinding
+import com.oluwafemi.findadev.ui.MainFragment
+import com.oluwafemi.findadev.ui.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        replaceFragment(MainFragment())
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.all_devs -> fragment = MainFragment()
+                R.id.reg_self -> fragment = RegisterFragment()
+            }
+            this@MainActivity.replaceFragment(fragment)
+        }
+
     }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment?): Boolean {
+    if (fragment != null) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.host_fragment, fragment)
+        transaction.commit()
+
+        return true
+    }
+    return false
 }
