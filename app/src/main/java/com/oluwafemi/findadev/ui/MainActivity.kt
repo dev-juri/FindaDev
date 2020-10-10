9f49139cd9de455f465051bbaf2e9437876fdd61
dev-juri <ojurifemi132@gmail.com>
2020-10-10 21:19:20 +0100

    fix: corrected annotation

--- app/src/main/java/com/oluwafemi/findadev/ui/MainActivity.kt
+++ app/src/main/java/com/oluwafemi/findadev/ui/MainActivity.kt
@@ -1,20 +1,19 @@
-package com.oluwafemi.findadev
+package com.oluwafemi.findadev.ui
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.oluwafemi.findadev.R
 import com.oluwafemi.findadev.databinding.ActivityMainBinding
-import com.oluwafemi.findadev.ui.MainFragment
-import com.oluwafemi.findadev.ui.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main
+        )
 
         replaceFragment(MainFragment())
 
