1fcdd94c1917164f41ce920e473cab539d9131a3
dev-juri <ojurifemi132@gmail.com>
2020-10-18 22:20:12 +0100

    chore: Refactoring, fixes and modifications

--- app/src/main/java/com/oluwafemi/findadev/model/Dev.kt
+++ app/src/main/java/com/oluwafemi/findadev/model/Dev.kt
@@ -1,9 +1,18 @@
 package com.oluwafemi.findadev.model
 
-data class Dev (
-    val fullName : String,
-    val devEmail : String,
-    val devPortfolio : String,
-    val devStack : String,
-    val jobType : String
-)
+import java.io.Serializable
+
+
+data class Dev(
+    val fullName: String = "",
+    val devEmail: String = "",
+    val devPortfolio: String = "",
+    val devStack: String = "",
+    val jobType: String = ""
+) : Serializable {
+
+    override fun toString(): String {
+        return "Interested in $jobType roles"
+    }
+}
+
--- app/src/main/java/com/oluwafemi/findadev/viewmodels/MainFragmentViewModel.kt
+++ app/src/main/java/com/oluwafemi/findadev/viewmodels/MainFragmentViewModel.kt
@@ -2,16 +2,55 @@ package com.oluwafemi.findadev.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.oluwafemi.findadev.model.Dev
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class MainFragmentViewModel(application: Application) : AndroidViewModel(application) {
     private val viewModelJob = SupervisorJob()
     private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+    private lateinit var firestoreInstance: FirebaseFirestore
+    private var _devList = MutableLiveData<ArrayList<Dev>>()
+    val devList: LiveData<ArrayList<Dev>>
+        get() = _devList
+    private val _status = MutableLiveData<UploadStatus>()
+    val status: LiveData<UploadStatus>
+        get() = _status
+
+    init {
+        firestoreInstance = FirebaseFirestore.getInstance()
+        getAllDevs()
+    }
 
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
+    }
+
+    private fun getAllDevs() {
+        viewModelScope.launch {
+            firestoreInstance.collection("devs").addSnapshotListener { value, error ->
+                if (error != null) {
+                    _status.value = UploadStatus.FAILURE
+                    return@addSnapshotListener
+                }
+                if (value != null) {
+                    val allUsers = ArrayList<Dev>()
+                    val documents = value.documents
+                    documents.forEach{
+                        val devFromDB = it.toObject(Dev::class.java)
+                        if (devFromDB != null) {
+                            allUsers.add(devFromDB)
+                        }
+                    }
+                    _devList.value = allUsers
+                }
+            }
+        }
     }
 }
